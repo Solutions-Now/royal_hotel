43f1544975aaa489fbe2cd3f5c5c61e2fa79bdb8
Sohib Al-Bakri <sohibbakri9@gmail.com>
2024-07-21 16:23:52 +0300

    home page

--- lib/modules/home/controller/home_controller.dart
+++ lib/modules/home/controller/home_controller.dart
@@ -1,3 +1,26 @@
 import '../../../resources/helpers/all_imports.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  List adsList = [
+    {
+      'title': 'الاعلان 1',
+      'description': 'وصف الاعلان 1',
+      'image': ImagesManager.ads1,
+    },
+    {
+      'title': 'الاعلان 2',
+      'description': 'وصف الاعلان 2',
+      'image': ImagesManager.ads2,
+    },
+    {
+      'title': 'الاعلان 3',
+      'description': 'وصف الاعلان 3',
+      'image': ImagesManager.ads3,
+    },
+    {
+      'title': 'الاعلان 4',
+      'description': 'وصف الاعلان 4',
+      'image': ImagesManager.ads4,
+    },
+  ];
+}
